80188eaee3ac4561f530479d9bfd67dcb7222a66
René Schönfelder <schoenfelder2211@gmail.com>
2023-03-14 13:37:14 +0100

    example on how to setup vue mapbox, key copied from: https://codesandbox.io/s/vuebabmapattemp1-usrgs?file=/src/App.vue:487-518

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import MyMap from './components/MyMap.vue'
+
+export default {
+  components: {
+    MyMap
+  }
+}
+
+</script>
+
+<template>
+  Hi <br>
+  <MyMap></MyMap>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MyMap.vue
@@ -0,0 +1,21 @@
+<script>
+  import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl';
+  import 'mapbox-gl/dist/mapbox-gl.css';
+
+  export default {
+    components: {
+        MapboxMap, MapboxMarker
+    }
+  }
+</script>
+
+<template>
+  <MapboxMap
+    style="height: 400px"
+    accessToken="pk.eyJ1IjoicnVzaGpzIiwiYSI6ImNrZ2J5cjB5bDBsb3gyenF1aWlvM3pteTIifQ.O_gv1VxHLDvC-UlG7dTYOA"
+    mapStyle="mapbox://styles/mapbox/streets-v11"
+    :center="[0, 0]"
+    :zoom="1">
+    <MapboxMarker :lng-lat="[50, 10]" />
+  </MapboxMap>
+</template>
